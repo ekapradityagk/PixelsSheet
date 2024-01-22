9c9b4f2daf8b7bbe536c24b04ccd53949065dbc0
ekapradityagk <ekapradityagk@gmail.com>
2024-01-22 18:11:40 +0700

    Initial Commit

---
+++ PixelsSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixelsSheet
+//
+//  Created by Eka Praditya on 22/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixelsSheet/PixelsSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelsSheetApp.swift
+//  PixelsSheet
+//
+//  Created by Eka Praditya on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PixelsSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
